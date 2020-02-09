81587e13216feeb53e3bd2d8ffc245c0d4434743
Gabriel Henrichs <gabrielhenrichs@gmail.com>
2020-02-09 16:22:43 -0300

    Inclusão de registro no endpoint posts

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -3,6 +3,7 @@ export default (req, res) => {
         posts: [
             { title: 'Título 1', content: 'lksfj askldfj alskdjf a'},
             { title: 'Título 2', content: 'lksfj askldfj alskdjf a'},
+            { title: 'Título 3', content: 'Conteúdo askldfj alskdjf a'},
         ]
     });
   };
