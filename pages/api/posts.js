ff9c7ca9cfb2705e130a6d9c1dcb7b31e1d3ebf8
Gabriel Henrichs <gabrielhenrichs@gmail.com>
2020-02-09 16:12:39 -0300

    Teste de endpoint para mock de postagens

---
+++ pages/api/posts.js
@@ -0,0 +1,9 @@
+export default (req, res) => {
+    res.json({
+        posts: [
+            { title: 'Título 1', content: 'lksfj askldfj alskdjf a'},
+            { title: 'Título 2', content: 'lksfj askldfj alskdjf a'},
+        ]
+    });
+  };
+  
