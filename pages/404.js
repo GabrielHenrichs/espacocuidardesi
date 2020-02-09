68b4922614ca84b4cbf21b548b48d15f87df001d
Gabriel Henrichs <gabrielhenrichs@gmail.com>
2020-02-09 16:07:31 -0300

    Alterações iniciais

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = function() {
+    return (
+        <div>
+            Esta página não existe...
+        </div>
+    )
+}
+
+export default NotFoundPage
