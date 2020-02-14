a1a76f23c8d07791131060de50af17295c4208e3
Gabriel Henrichs <gabrielhenrichs@gmail.com>
2020-02-13 23:00:02 -0300

    Inclusão logo do navbar e next-images

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import LogoNav from '../src/img/logonav.png'
 
 const Nav = () => (
   <div className="container mb-3">
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light">
       <a className="navbar-brand text-warning" href="#">
-        {/* <img src={Logo} alt="Logo Cuidar de Si" /> */}
+        <img src={LogoNav} />
       </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
