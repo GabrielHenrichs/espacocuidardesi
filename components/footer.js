351a5d8c273cf6449cf3068fefc8af44d98008e2
Gabriel Henrichs <gabrielhenrichs@gmail.com>
2020-02-25 11:22:52 -0300

    Melhora na responsividade do nav

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const Footer = () => (
-    <div className="footer mt-auto py-2 bg-light">
+    <div className="footer navbar-fixed-bottom mt-auto py-2 bg-light">
         <div className="container">
             <p className="text-muted align-middle text-center mb-1">
                 &copy; Copyright 2020 Cuidar de Si - Todos os direitos reservados
--- components/nav.js
+++ components/nav.js
@@ -3,16 +3,13 @@ import LogoNav from '../src/img/logonav.png'
 import "../styles/styles.scss"
 
 const Nav = () => (
-  <div className="bg-white mb-3 shadow-sm">
-    <nav className="container navbar navbar-expand-md">
-      <a className="navbar-brand col-6 col-md-3 col-lg-3" href="/index">
+  <div className="container bg-white mb-3 shadow-sm">
+    <nav className=" navbar navbar-expand-lg">
+      <a className="navbar-brand col-6 col-lg-3" href="/index">
         <img src={LogoNav} />
       </a>
-      <button className="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon col-3"></span>
-      </button>
 
-      <div className="collapse navbar-collapse col-md-9 col-lg-6 row align-items-center" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse col-lg-6 row align-items-center" id="navbarSupportedContent">
         <ul className="mx-auto navbar-nav">
           <li className="nav-item">
             <a className="nav-link logo-text rounded btn-outline-light" href="/index">INÍCIO <span className="sr-only">(current)</span></a>
@@ -29,11 +26,15 @@ const Nav = () => (
         </ul>
       </div>
 
-      <div className="collapse navbar-collapse col-lg-3 text-right align-items-center">
+      <div className="row col-4 col-lg-3 text-right align-items-center">
         <a className="logo-rs col-4" href="https://www.facebook.com/espacocuidardesi/" target="_blank"><i class="fab fa-facebook-square"></i></a>
         <a className="logo-rs col-4" href="https://www.instagram.com/espacocuidardesi/" target="_blank"><i className="fab fa-instagram-square"></i></a>
         <a className="logo-rs col-4" href="https://api.whatsapp.com/send?1=pt_BR&phone=5532991757257" target="_blank"><i class="fab fa-whatsapp-square"></i></a>
       </div>
+      
+      <button className="navbar-toggler navbar-light col-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
     </nav>
   </div>
 );
