8cd058fbc23a6b9567455613990a80550c4dd898
Gabriel Henrichs <gabrielhenrichs@gmail.com>
2020-02-25 10:47:22 -0300

    Alterações no nav

--- components/nav.js
+++ components/nav.js
@@ -5,15 +5,15 @@ import "../styles/styles.scss"
 const Nav = () => (
   <div className="bg-white mb-3 shadow-sm">
     <nav className="container navbar navbar-expand-md">
-      <a className="navbar-brand col-9 col-md-3 col-lg-3" href="/index">
+      <a className="navbar-brand col-6 col-md-3 col-lg-3" href="/index">
         <img src={LogoNav} />
       </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon col-3"></span>
       </button>
 
-      <div className="collapse navbar-collapse text col-md-9 col-lg-6" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto mx-auto">
+      <div className="collapse navbar-collapse col-md-9 col-lg-6 row align-items-center" id="navbarSupportedContent">
+        <ul className="mx-auto navbar-nav">
           <li className="nav-item">
             <a className="nav-link logo-text rounded btn-outline-light" href="/index">INÍCIO <span className="sr-only">(current)</span></a>
           </li>
@@ -29,9 +29,10 @@ const Nav = () => (
         </ul>
       </div>
 
-      <div className="col-lg-3">
-        <a className="logo-fa mx-auto" href="/index"><i className="fab fa-instagram-square"></i></a>
-        <a className="logo-fa mx-auto" href="/index"><i class="fab fa-facebook-square"></i></a>
+      <div className="collapse navbar-collapse col-lg-3 text-right align-items-center">
+        <a className="logo-rs col-4" href="https://www.facebook.com/espacocuidardesi/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+        <a className="logo-rs col-4" href="https://www.instagram.com/espacocuidardesi/" target="_blank"><i className="fab fa-instagram-square"></i></a>
+        <a className="logo-rs col-4" href="https://api.whatsapp.com/send?1=pt_BR&phone=5532991757257" target="_blank"><i class="fab fa-whatsapp-square"></i></a>
       </div>
     </nav>
   </div>
