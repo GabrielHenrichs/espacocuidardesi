ed8d4bdc5db507bb353031f3ce8066a39347134b
Gabriel Henrichs <gabrielhenrichs@gmail.com>
2020-03-02 17:42:23 -0300

    Melhorias em Contato 2

--- pages/contato.js
+++ pages/contato.js
@@ -23,27 +23,29 @@ const Contact = () => {
           </div>
         </section>
 
-        <section className="container col-5 my-3 px-4 pt-2 img-thumbnail bg-light ">
-          <div className="border-bottom py-2">
-            <h4 className="font-weight-normal"><i className="fas fa-phone"></i> Telefone:</h4>
-            <p className="text-center">(32)3321-0619 | (32)99175-7257</p>
-          </div>
-          <div className="border-bottom py-2">
-            <h4 className="font-weight-normal"><i className="fas fa-map-marker-alt"></i> Endereço:</h4>
-            <p className="mb-1 text-center">Rua Machado Sobrinho, 277 - Alto dos Passos</p>
-            <p className="text-center">Juiz de Fora, MG</p>
-          </div>
-          <div className="border-bottom py-2">
-            <h4 className="font-weight-normal"><i className="fas fa-at"></i> Email:</h4>
-            <p className="text-center">cuidardesi.jf@gmail.com</p>
-          </div>
-          <div className="py-2">
-            <h4 className="font-weight-normal"><i className="fas fa-hashtag"></i> Redes Sociais:</h4>
-            <p className="text-center"><a href="facebook.com/espacocuidardesi" className="text-dark"><i className="fab fa-facebook-square"></i> facebook.com/espacocuidardesi</a></p>
-            <p className="text-center"><a href="instagram.com/espacocuidardesi" className="text-dark"><i className="fab fa-instagram-square"></i> instagram.com/espacocuidardesi</a></p>
-            <p className="text-center"><a href="https://api.whatsapp.com/send?1=pt_BR&phone=5532991757257" className="text-dark"><i class="fab fa-whatsapp-square"></i> Whatsapp: (32)99175-7257</a></p>
-          </div>
-        </section>
+        <div className="container row d-flex justify-content-center text-center mx-auto">
+          <section className="col-12 col-md-6 my-3 px-4 pt-2 img-thumbnail bg-light ">
+            <div className="border-bottom py-2">
+              <h4 className="font-weight-normal"><i className="fas fa-phone"></i> Telefone:</h4>
+              <p className="text-center">(32)3321-0619 | (32)99175-7257</p>
+            </div>
+            <div className="border-bottom py-2">
+              <h4 className="font-weight-normal"><i className="fas fa-map-marker-alt"></i> Endereço:</h4>
+              <p className="mb-1 text-center">Rua Machado Sobrinho, 277 - Alto dos Passos</p>
+              <p className="text-center">Juiz de Fora, MG</p>
+            </div>
+            <div className="border-bottom py-2">
+              <h4 className="font-weight-normal"><i className="fas fa-at"></i> Email:</h4>
+              <p className="text-center">cuidardesi.jf@gmail.com</p>
+            </div>
+            <div className="py-2">
+              <h4 className="font-weight-normal"><i className="fas fa-hashtag"></i> Redes Sociais:</h4>
+              <p className="text-center"><a href="facebook.com/espacocuidardesi" className="text-dark"><i className="fab fa-facebook-square"></i> facebook.com/espacocuidardesi</a></p>
+              <p className="text-center"><a href="instagram.com/espacocuidardesi" className="text-dark"><i className="fab fa-instagram-square"></i> instagram.com/espacocuidardesi</a></p>
+              <p className="text-center"><a href="https://api.whatsapp.com/send?1=pt_BR&phone=5532991757257" className="text-dark"><i class="fab fa-whatsapp-square"></i> Whatsapp</a></p>
+            </div>
+          </section>
+        </div>
       </main>
 
       <footer>
